71243ecc7d25a92357dbe937c10f6e8b359601b8
gabrielleite16 <36733599+gabrielleite16@users.noreply.github.com>
2018-10-23 19:59:52 -0300

    Add files via upload

---
+++ lista2_GabrielLeite/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace lista2_GabrielLeite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaPoupanca poupador = new ContaPoupanca(2000);
+            ContaPoupanca poupador2 = new ContaPoupanca(3000);
+
+            ContaPoupanca.AlterarTaxaDeJuros(0.04);
+
+            Console.Write("*Juros de 4%\n\n");
+
+            Console.Write("Novo Saldo do poupador: R$ " + poupador.CalcularJuroMensal());
+
+            Console.Write("\nNovo Saldo do poupador2: R$ " + poupador2.CalcularJuroMensal());
+
+            ContaPoupanca.AlterarTaxaDeJuros(0.05);
+
+            Console.Write("\n\n\n*Juros de 5%\n\n");
+
+            Console.Write("Novo Saldo do poupador: R$ " + poupador.CalcularJuroMensal());
+
+            Console.Write("\nNovo Saldo do poupador2: R$ " + poupador2.CalcularJuroMensal());
+
+            Console.ReadKey();
+        }
+    }
+}
